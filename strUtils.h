604f973cceecbffa65f2697b61154dff5ce51b1c
luan-brav0 <luan-brav0@github.com>
2024-09-05 20:32:10 -0300

    compiled stuff

--- strUtils.h
+++ strUtils.h
@@ -1,7 +1,7 @@
 #ifndef STRUTILS_H
 #define STRUTILS_H
 
-int strLen(char*s);
+int strLen(char* s);
 int strEnd(char* s, char* t);
 char* strCat(char* s, char* t);
 char* strCpy(char*s, char* t, int n);
