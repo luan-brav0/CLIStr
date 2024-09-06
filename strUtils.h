9a7f829482480966469c88d9a7d2e2b9929677cf
luan-brav0 <luan-brav0@github.com>
2024-09-06 18:26:36 -0300

    strLen return type corrected in header file

--- strUtils.h
+++ strUtils.h
@@ -1,9 +1,8 @@
 #ifndef STRUTILS_H
 #define STRUTILS_H
 
-int strLen(char* s);
+size_t strLen(char* s);
 int strEnd(char* s, char* t);
 char* strCat(char* s, char* t);
-char* strCpy(char*s, char* t, int n);
 
 #endif
